9b7f475915804387c77cf18c0b8e244142eeff73
Meryeme <meriemsalma34@gmail.com>
2024-08-11 17:03:20 +0100

    adding the validation

--- Backend/Application/Services/AuthenticationAndAuthorization/Validators/DeleteUserCommandValidator.cs
+++ Backend/Application/Services/AuthenticationAndAuthorization/Validators/DeleteUserCommandValidator.cs
@@ -8,19 +8,12 @@ using System.Threading.Tasks;
 
 namespace Application.Services.AuthenticationAndAuthorization.Validators
 {
-    using FluentValidation;
-
-    public class AssignRoleCommandValidator : AbstractValidator<AssignRoleCommand>
+    public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
     {
-        public AssignRoleCommandValidator()
+        public DeleteUserCommandValidator()
         {
             RuleFor(x => x.UserId)
                 .NotEmpty().WithMessage("UserId is required.");
-
-            RuleFor(x => x.Role)
-                .NotEmpty().WithMessage("Role is required.")
-                .Matches("^[a-zA-Z0-9]+$").WithMessage("Role must be alphanumeric.");
         }
     }
-
 }
