ad87b56f1b060944cae8fd28f83e6a562dcf2a3d
Meryeme <meriemsalma34@gmail.com>
2024-08-12 09:47:00 +0100

    some changes related to  the change email feature

---
+++ Backend/Domain/DTOs/ChangeEmailResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs
+{
+    public record ChangeEmailResponse(bool Success, string Message, string[] Errors = null);
+
+}
