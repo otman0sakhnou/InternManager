6fe3a0a599e6ea2bb1a4bdfce22f9a0c169ae4ed
Meryeme <meriemsalma34@gmail.com>
2024-08-19 13:41:32 +0100

    create 3 endpoints one to validate steps one to get details for subject and a group one to get details for an intern and a goup

---
+++ Backend/Infrastructure/Repositories/InternStepRepository.cs
@@ -0,0 +1,50 @@
+﻿using Application.Repositories;
+using Domain.Models;
+using Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class InternStepRepository : IInternStepRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InternStepRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(InternStep internStep)
+        {
+            await _context.InternSteps.AddAsync(internStep);
+        }
+
+        public async Task<IEnumerable<InternStep>> GetByInternIdAsync(Guid internId)
+        {
+            return await _context.InternSteps
+                .Where(internStep => internStep.InternId == internId)
+                .Include(internStep => internStep.Step) 
+                .Include(internStep => internStep.Intern) 
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<InternStep>> GetByStepIdAsync(Guid stepId)
+        {
+            return await _context.InternSteps
+                .Where(internStep => internStep.StepId == stepId)
+                .Include(internStep => internStep.Step)
+                .Include(internStep => internStep.Intern)
+                .ToListAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
