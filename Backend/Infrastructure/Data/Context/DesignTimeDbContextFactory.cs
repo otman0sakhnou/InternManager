27761be85a6be79e32e417acbb9f41c57da5c618
Haddouchesara <haddouchesara89@gmail.com>
2024-08-23 00:58:11 +0100

    Refactor master branch for correct operation
    Refactor master branch for correct operation

--- Backend/Infrastructure/Data/Context/DesignTimeDbContextFactory.cs
+++ Backend/Infrastructure/Data/Context/DesignTimeDbContextFactory.cs
@@ -10,7 +10,11 @@ namespace Infrastructure.Data.Context
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             // Utilisez la chaîne de connexion appropriée pour votre environnement de conception
+<<<<<<< HEAD
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-KC3KUH9\\SQLEXPRESS;Initial Catalog=InternManagerDb;Integrated Security=True;TrustServerCertificate=True");
+=======
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-L1KJOGA\\SQLEXPRESS01;Initial Catalog=InternManagerDb2;Integrated Security=True;TrustServerCertificate=True");
+>>>>>>> 58a8276 (Refactor master branch for correct operation)
 
             return new ApplicationDbContext(optionsBuilder.Options);
         }
