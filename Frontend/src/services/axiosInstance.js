868b4ebe91932374525d0c389237070061f0de3b
Meryeme <meriemsalma34@gmail.com>
2024-08-22 12:39:24 +0100

    adding the authorization to the front project

---
+++ Frontend/src/services/axiosInstance.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: REACT_APP_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
+
